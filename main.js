63c04be3dba28a1de4e26c26b78d966864cfdef5
Nathan Riemer <nathan.riemer@gmail.com>
2015-11-05 14:37:48 -0500

    turn logic coming along nicely

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var $board = $('.board');
 
 var turn = [];
 
+var currentGame;
+
+var $turnCounter = $('#turns');
+
 var Game = function(numPairs) {
 	this.numPairs = numPairs;
 	this.turns = 0;
@@ -38,8 +42,12 @@ Card.prototype.flip = function() {
 
 Card.prototype.place = function() {
 	$board.append(this.$div);
+	this.activate();
+};
+
+Card.prototype.activate = function() {
 	var card = this;
-	this.$div.on('click', function() {
+	this.$div.one('click', function() {
 		card.choose();
 	});
 };
@@ -53,11 +61,25 @@ Card.prototype.choose = function() {
 };
 
 var evaluateTurn = function() {
-	//
+	if (turn[0].value === turn[1].value) {
+		console.log('MATCH!');
+		//TODO: flash a message in the match display div instead
+	} else {
+		// I THINK I NEED A CLOSURE HEEEEEERE
+		// window.setTimeout(function() {
+		turn[0].flip();
+		turn[0].activate();
+		turn[1].flip();
+		turn[1].activate();
+		// }, 2000);
+	}
+	currentGame.turns++;
+	$turnCounter.text(currentGame.turns);
+	turn = [];
 };
 
 
 
-var newGame = new Game(6);
-newGame.getCards();
-newGame.deal();
+currentGame = new Game(6);
+currentGame.getCards();
+currentGame.deal();
